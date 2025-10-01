855ca74e09a8fa98a0521b1acd83c8835726698e
sai.rane <sai.rane729@gmail.com>
2025-10-01 18:37:31 +0530

    styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import "primeicons/primeicons.css"; // Icons
+import "primereact/resources/primereact.min.css"; // Core
+import "primereact/resources/themes/lara-light-blue/theme.css"; // Theme
 import "./App.css";
 import List from "./components/List/List";
-import "primereact/resources/themes/lara-light-blue/theme.css"; // Theme
-import "primereact/resources/primereact.min.css"; // Core
-import "primeicons/primeicons.css"; // Icons
 
 function App() {
   return (
